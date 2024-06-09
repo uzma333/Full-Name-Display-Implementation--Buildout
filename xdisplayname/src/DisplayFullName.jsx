8ce0d384f8f3c9a711f0a4231005ea6aa6622ca1
noor uzma <uzmasalmaan23@gmail.com>
2024-06-09 21:57:56 +0530

    added validation

--- xdisplayname/src/DisplayFullName.jsx
+++ xdisplayname/src/DisplayFullName.jsx
@@ -1,4 +1,4 @@
-import { useState ,useRef} from "react";
+import { useState} from "react";
 import "./DisplayFullName.css";
 
 const FullName=()=>{
@@ -6,7 +6,9 @@ const FullName=()=>{
 const[firstname,setFirstname]=useState("");
 const[lastname,setLastname]=useState("");
 const[fullname,setFullname]=useState("");
-const inputRef=useRef(null);
+const[validation,setValidation]=useState("");
+
+
 
 
 const handleFirstName=(e)=>{
@@ -17,30 +19,36 @@ const handleLastName=(e)=>{
 }
 
 
-const handleSubmit=()=>{
+
+const handleValidation=(e)=>{
+    e.preventDefault();
+    if(!firstname || !lastname){
+        setValidation("please enter the field")
+    }else{
     setFullname(`${firstname} ${lastname}`)
-    inputRef.current.focus();
-    
+    setValidation("");
 }
+}
+
 
 
     return (
 <div className="container">
     <h1>Full Name Display</h1>
-   <div>
-   <label for="firstname">First Name:</label>
-   <input type="text" id="firstname" name="firstname" onChange={handleFirstName} ref={inputRef}/>
-   </div>
+    <form onSubmit={handleValidation}>
         <div>
+   <label for="firstname">First Name:</label>
+   <input type="text" id="firstname" name="firstname" onChange={handleFirstName} required pattern="[A-Za-z]{3,}" title="Three or more letters"/>
+   </div>
+  
+       <div>
             <label for="lastname">Last Name:</label>
-        <input type="text" id="lastname" name="lastname" onChange={handleLastName}/>
-        </div>
-        <button onClick={handleSubmit}>Submit</button>
-        
-    
-        
-   
-    <p>Full Name: {fullname} </p>
+        <input type="text" id="lastname" name="lastname" onChange={handleLastName} required pattern="[A-Za-z]{3,}" title="Three or more letters"/>
+       </div>
+       <button>Submit</button>
+        </form> 
+        {validation && <p className="validate">{validation}</p>}
+        <p>Full Name: {fullname} </p>
 </div>
     )
 }
